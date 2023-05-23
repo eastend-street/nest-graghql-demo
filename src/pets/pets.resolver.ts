4a45d0fdcd775576ef416bf0c197cecd87cc6e8f
eastend-street <suminari.yamada@gmail.com>
2023-05-24 00:09:24 +0900

    feat: get ownerId in Pet resolver using @parent

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  Parent,
+  ResolveField,
+} from '@nestjs/graphql';
 import { PetsService } from './pets.service';
 import { Pet } from './pets.entity';
 import { CreatePetInput } from './dto/create-pet.input';
+import { Owner } from 'src/owners/entities/owner.entity';
 
 @Resolver((of) => Pet)
 export class PetsResolver {
@@ -15,6 +24,12 @@ export class PetsResolver {
   @Query((returns) => [Pet])
   pets(): Promise<Pet[]> {
     return this.petsService.findAll();
+  }
+
+  // To get the ownerId from the parent query(pet), use @Parent to access the ownerId
+  @ResolveField((returns) => Owner)
+  owner(@Parent() pet: Pet): Promise<Owner> {
+    return this.petsService.getOwner(pet.ownerId);
   }
 
   @Mutation((returns) => Pet)
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -3,10 +3,15 @@ import { Pet } from './pets.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePetInput } from './dto/create-pet.input';
+import { OwnersService } from 'src/owners/owners.service';
+import { Owner } from 'src/owners/entities/owner.entity';
 
 @Injectable()
 export class PetsService {
-  constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
+  constructor(
+    @InjectRepository(Pet) private petsRepository: Repository<Pet>,
+    private ownersService: OwnersService,
+  ) {}
 
   createPet(createPetInput: CreatePetInput): Promise<Pet> {
     const newPet = this.petsRepository.create(createPetInput); // newPet = new Pet(); new.name = input.name
@@ -20,5 +25,9 @@ export class PetsService {
 
   findOne(id: number): Promise<Pet> {
     return this.petsRepository.findOneOrFail({ where: { id } });
+  }
+
+  getOwner(ownerId: number): Promise<Owner> {
+    return this.ownersService.findOne(ownerId);
   }
 }
